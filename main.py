6f1a82701eb432545e378433e5f33d259ba91c3f
Veronika <vkotsubiak@gmail.com>
2022-10-15 16:34:58 +0300

    опції

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ number1 = float(input("Веддіть перше число:"))
 deia = input("Веддіть знак: ")
 number2 = float(input("Веддіть друге число:"))
 
+if deia == '+':
+    print(number1 + number2)
+elif deia == '-':
+    print(number1 - number2)
+elif deia == '*':
+    print(number1 * number2)
+elif deia == '/':
+    print(number1 / number2)
+else:
+    print("дія не можлива")
+
